e4f88a96c4e0b82e636deeaff898a7be905102b8
Amit Barve <mtbar131@gmail.com>
2017-10-16 15:25:00 -0400

    infrastructure setup

--- Resources/deploy.sh
+++ Resources/deploy.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-set -x
+# set -x
 
 # GITHUB_TOKEN=d5b4a6611363172dce6dee49cf9db293cfac6fde
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 USER IP-ADDRESS" >&2
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 USER IP-ADDRESS BRANCH" >&2
   exit 1
 fi
 
 USER=$1
 IP=$2
+BRANCH=$3
+
+echo "USER: ${USER}, IP: ${IP}, BRANCH: ${BRANCH}"
 
 # copy dockerfile to machine
-scp Dockerfile ${USER}@${IP}:/home/${USER}
+# scp Dockerfile ${USER}@${IP}:/home/${USER}
 
-ssh -t -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no"  ${USER}@${IP} <<'EOF'
+ssh -t -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" ${USER}@${IP} IP=${IP} BRANCH=${BRANCH} 'bash -s' << 'EOF'
+
+echo "USER: ${USER}, IP: ${IP}, BRANCH: ${BRANCH}"
 
 sudo chown -R ${USER} .
 
-echo "Running on: " `hostname`
+echo "Running on: " `hostname` "with user: ${USER}"
 
 sleep 2
 
 # clone the git repo
 git clone https://d5b4a6611363172dce6dee49cf9db293cfac6fde@github.ncsu.edu/aabarve/chronstore.git
+cd chronstore
+git checkout ${BRANCH}
 
 # STEP: install docker
 # add repo
@@ -38,10 +45,31 @@ sudo apt-get install -y maven git emacs24 \
      software-properties-common \
      docker-ce
 
+cd ..
+
+mkdir logs
+# Get config files into logs
+cp chronstore/Resoruces/log_server/* .
+cd logs
+
+# Get log4j jar
+wget http://central.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar
+
+# start all 3 logging services
+java -cp log4j-1.2.17.jar org.apache.log4j.net.SimpleSocketServer 4712 log4j.Chord.properties &
+java -cp log4j-1.2.17.jar org.apache.log4j.net.SimpleSocketServer 4713 log4j.KeyStore.properties &
+java -cp log4j-1.2.17.jar org.apache.log4j.net.SimpleSocketServer 4714 log4j.Analysis.properties &
+
+cd ..
+
+# get Dockerfile
+cp chronstore/Resources/Dockerfile .
+
 # Build the image
 sudo docker build -t chronstore .
 
 # Run the container - this should go in drun
 sudo docker run -v ~/chronstore:/root/chronstore chronstore
+
 
 EOF
--- Resources/docker_scripts/drun.sh
+++ Resources/docker_scripts/drun.sh
@@ -4,5 +4,5 @@ set -x
 for i in `seq 1 $1`
 do
     echo "$i"
-    sudo docker run chronstore -v ~/chronstore:/root/chronstore &
+    sudo docker run -v ~/chronstore:/root/chronstore chronstore &
 done
