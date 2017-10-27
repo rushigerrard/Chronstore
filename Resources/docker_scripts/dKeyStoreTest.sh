7b0ee557bea93815dbe783f1a378f2afae896b50
Amit Barve <mtbar131@gmail.com>
2017-10-27 08:36:06 -0400

    bug fix in dKeyStoreTest.sh

--- Resources/docker_scripts/dKeyStoreTest.sh
+++ Resources/docker_scripts/dKeyStoreTest.sh
@@ -6,4 +6,4 @@ set -x
 sudo python ~/chronstore/Resources/docker_scripts/FetchIPAddress.py
 
 # run maven tests
-sudo docker run -v ~/chronstore:/root/chronstore chronstore bash -c "cd /root/chronstore/Chord;mvn clean install;cd /root/chronstore/ObjectStore;mvn clean install;cd /root/chronstore/Client;mvn exec:java -Dexec.args=\"$1 $2\""
+sudo docker run -v ~/chronstore:/root/chronstore chronstore bash -c "cd /root/chronstore/Chord;mvn clean install;cd /root/chronstore/ObjectStore;mvn clean install;cd /root/chronstore/Client;mvn compile exec:java -Dexec.args=\"$1 $2\""
